4e3d66fe54b9059950422c5e81e92783c0d86193
softabhi <kdhar499@gmail.com>
2025-03-13 04:31:36 +0530

    delete problem fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -89,8 +89,9 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect('/products')->with('status','category deleted succesfully');
     }
 }
